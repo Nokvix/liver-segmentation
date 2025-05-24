6f442b1356bf18b2c68d4a190059079f891146a9
Alexey Yurkov <yessirskill@yandex.ru>
2025-05-24 16:58:43 +0500

    update backend structure

--- backend/backend-ml/src/main/java/com/example/backend_ml/web/dto/request/SegmentationRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend_ml.web.dto.request;
-
-import lombok.Data;
-
-@Data
-public class SegmentationRequest {
-    private String filePath;    // Пример: "studies/liver.nii.gz"
-    private int sliceIndex;     // Пример: 35
-}
